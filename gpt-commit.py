7b1a501784b93747ddb787fc79cf934f40b29b6f
markuswt <53354649+markuswt@users.noreply.github.com>
2023-01-31 19:37:30 +0000

    Fix commit whitespace handling

--- gpt-commit.py
+++ gpt-commit.py
@@ -9,76 +9,71 @@ COMMIT_MSG_PROMPT = "Generate a short commit message from this:"
 openai.organization = os.getenv("OPENAI_ORG_ID")
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-def complete(prompt):
-    completion_resp = openai.Completion.create(prompt=prompt[:4097], engine="text-davinci-003", max_tokens=64)
-    completion = completion_resp["choices"][0]["text"].strip()
 
+def complete(prompt):
+    completion_resp = openai.Completion.create(prompt=prompt[:10000],
+                                               engine="text-davinci-003",
+                                               max_tokens=128)
+    completion = completion_resp["choices"][0]["text"].strip()
     return completion
 
 
 def summarize_diff(diff):
     assert diff
-
     return complete(DIFF_PROMPT + "\n\n" + diff + "\n\n")
 
 
 def generate_commit_message(summaries):
     assert summaries
-
     return complete(COMMIT_MSG_PROMPT + "\n\n" + summaries + "\n\n")
 
 
 def get_diff(path=".", diff_filter="ACDMRTUXB", name_only=False):
     arguments = [
-        "git", "--no-pager", "diff", "--staged", f"--diff-filter={diff_filter}"
+        "git", "--no-pager", "diff", "--staged", "--ignore-space-change",
+        "--ignore-all-space", "--ignore-blank-lines",
+        f"--diff-filter={diff_filter}"
     ]
-
     if name_only:
         arguments.append("--name-only")
-
+    
     diff_process = subprocess.run(arguments + [path],
                                   capture_output=True,
                                   text=True)
-
     diff_process.check_returncode()
-
     return diff_process.stdout.strip()
 
 
 def summarize_added_modified():
     modified_files = get_diff(name_only=True, diff_filter="AM").splitlines()
-
     return "\n\n".join(
         [summarize_diff(get_diff(file)) for file in modified_files])
 
 
 def summarize_deleted():
     deleted_files = get_diff(name_only=True, diff_filter="D").splitlines()
-
     return f"This change deletes files {', '.join(deleted_files)}" if deleted_files else ""
 
 
 def summarize_other():
     other_changes = get_diff(diff_filter="CRTUXB")
-
     return summarize_diff(other_changes) if other_changes else ""
 
+
 def commit(message):
-    subprocess.run(["git", "commit", "--message", message,
-                    "--edit"]).check_returncode()
+    return subprocess.run(["git", "commit", "--message", message,
+                           "--edit"]).returncode
+
 
 if __name__ == "__main__":
     diff = get_diff()
-
     if not diff:
-        print("Nothing to commit")
-    elif len(diff) < 11900:
+        # no files staged or only whitespace diffs
+        commit_message = "Fix whitespace"
+    elif len(diff) < 9900:
         commit_message = generate_commit_message(summarize_diff(diff))
-
-        commit(commit_message)
     else:
         summaries = summarize_added_modified() + "\n\n" + summarize_deleted(
         ) + "\n\n" + summarize_other()
         commit_message = generate_commit_message(summaries)
-
-        commit(commit_message)
+    exit(commit(commit_message))
